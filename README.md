a1bc822a481250056c123848d09f9a33da49add2
zzxxsix <129380150+zzxxsix@users.noreply.github.com>
2023-05-13 00:04:56 +0800

    add

---
+++ README.md
@@ -0,0 +1,43 @@
+
+# alemon-bot随机图片小插件
+
+## 介绍
+alemon-bot的TS小插件，ts文件放到`alemon-bot/example`文件夹即可使用
+有涩涩哦~
+
+
+## 安装教程
+
+
+```
+git clone https://gitee.com/zzxxsix/zzxxsix-plugin.git 
+
+```
+
+## 插件  setu.ts功能
+
+> `/tag` ， 二次元图片，可后面接标签，比如/tag萝莉  
+
+> `/r18` ， r18+二次元图片，可后面接标签，需要自己删  
+
+> `/二次元`， 随机二次元图片  
+
+> `/看看腿` ，看白丝，看黑丝  
+
+> `/三次元 ， 随机三次元图片  
+
+> `/三次元r18` ， 随机三次元r18+图片  
+
+
+
+# 免责声明
+
+1. 功能仅限内部交流与小范围使用，请勿将此插件用于以盈利为目的的场景
+2. 本插件仅供交流学习使用，如有侵权请联系，会立即删除
+3. 使用本插件造成的一切损失，以及不良影响，由使用者承担
+
+alemon-bot仓库：[☞Github](https://github.com/ningmengchongshui/alemon-bot)/[☞Gitee](https://gitee.com/ningmengchongshui/alemon-bot)
+
+
+
+
---
+++ setu.ts
@@ -0,0 +1,166 @@
+import plugin from '../src/lib/plugins'
+import {
+	EType,
+	Messgetype,
+	PluginType
+} from '../src/lib/types'
+
+
+export class ercy extends plugin {
+	[parameter: string]: PluginType
+	constructor() {
+		super({
+			dsc: '二次元图片',
+			rule: [{
+					reg: "^/tag.*$", //二次元图片，可后面接标签，比如/tag萝莉
+					fnc: 'tag'
+				},
+				{
+					reg: "^/r18.*$", //r18+二次元图片，可后面接标签，需要自己删
+					fnc: 'setu'
+				},
+				{
+					reg: "^/二次元.*$",
+					fnc: 'eryc' //随机二次元图片
+				},
+				{
+					reg: "^/看看腿.*$", //看白丝，看黑丝
+					fnc: 'kkt'
+				},
+				{
+					reg: "^/三次元.*$",
+					fnc: 'sanyc' //随机三次元图片
+				},
+				{
+					reg: "^/三次元r18.*$",
+					fnc: 'sanycr18' //随机三次元r18+图片
+				},
+			]
+		})
+	}
+
+
+	async tag(e: Messgetype) {
+
+		const tagContent = e.msg.content.replace(/^\/tag\s*/, '');
+		console.log(tagContent);
+
+		let url;
+		for (let i = 0; i < 5; i++) {
+			try {
+				let url = `https://api.lolicon.app/setu/v2?tag=${tagContent}&siae=small`;
+				const response = await fetch(url);
+				const data = await response.json(); // 解析 JSON 数据
+				const imageUrl = data.data[0].urls.original;
+				console.log(imageUrl);
+
+				e.reply(segment.at(e.msg.author.id), segment.image(imageUrl))
+			} catch (error) {
+				let urla = `https://api.lolicon.app/setu/v2?siae=small`;
+				const response = await fetch(urla);
+				const data = await response.json(); // 解析 JSON 数据
+				const imageUrla = data.data[0].urls.original;
+				console.log(imageUrla);
+
+				e.reply(segment.at(e.msg.author.id), segment.image(imageUrla))
+			}
+			return true
+		}
+	}
+
+
+	async setu(e: Messgetype) {
+        const tagContent = e.msg.content.replace(/^\/r18\s*/, '');
+		console.log(tagContent);
+
+		for (let i = 0; i < 3; i++) {
+			const randomNum = Math.floor(Math.random() * 2) + 1;
+			if (randomNum == 1) {
+
+				let url = `https://api.lolicon.app/setu/v2?tag=${tagContent}&siae=small&r18=1`;
+				const response = await fetch(url);
+				const data = await response.json(); // 解析 JSON 数据
+				const imageUrl = data.data[0].urls.original;
+				console.log(imageUrl);
+
+				e.reply(segment.at(e.msg.author.id), segment.image(imageUrl));
+			} else {
+				let urla = `https://www.acy.moe/api/r18`;
+				let res = await fetch(urla);
+
+				console.log(res.url);
+
+				e.reply(segment.at(e.msg.author.id), segment.image(res.url));
+			}
+
+		}
+		return true;
+	}
+
+
+	async eryc(e) {
+		const urls = [
+			"https://moe.jitsu.top/img",
+			"http://www.yzxdd.icu/api/",
+			"https://api.caonm.net/api/dm/index.php",
+			"https://image.anosu.top/pixiv/direct?size=small"
+		];
+
+		for (let i = 0; i < 3; i++) {
+			const randomNum = Math.floor(Math.random() * urls.length);
+			const res = await fetch(urls[randomNum]);
+			e.reply(segment.at(e.msg.author.id), segment.image(res.url));
+		}
+
+		return true;
+	}
+
+	async kkt(e) {
+		const urls = [
+			"https://api.caonm.net/api/bhs/b.php",
+			"https://api.caonm.net/api/bhs/h.php"
+		];
+
+		for (let i = 0; i < 4; i++) {
+			const randomNum = Math.floor(Math.random() * urls.length);
+			const res = await fetch(urls[randomNum]);
+			e.reply(segment.at(e.msg.author.id), segment.image(res.url));
+		}
+
+		return false;
+	}
+
+
+	async sanyc(e) {
+		const urls = [
+			"https://api.caonm.net/api/mnt/index.php",
+			"https://api.vvhan.com/api/girl",
+			"http://dsyai.club/api/youhuotu.php",
+			"https://api.caonm.net/api/mnt/index.php"
+		];
+
+		for (let i = 0; i < 3; i++) {
+			const randomNum = Math.floor(Math.random() * urls.length);
+			const res = await fetch(urls[randomNum]);
+			e.reply(segment.at(e.msg.author.id), segment.image(res.url));
+		}
+
+		return true;
+	}
+	async sanycr18(e) {
+		const urls = [
+			"https://imagesapi.sesepic.top/3r18",
+			"https://imagesapi.sesepic.top/cos"
+		];
+
+		for (let i = 0; i < 3; i++) {
+			const randomNum = Math.floor(Math.random() * urls.length);
+			const res = await fetch(urls[randomNum]);
+			e.reply(segment.at(e.msg.author.id), segment.image(res.url));
+		}
+
+		return true;
+	}
+
+
+}
