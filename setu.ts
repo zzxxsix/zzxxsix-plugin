56f9a9f7dcec77941b5323e7ab908710bf7771ef
zzxxsix <129380150+zzxxsix@users.noreply.github.com>
2023-06-26 20:43:03 +0800

    Update setu.ts

--- setu.ts
+++ setu.ts
@@ -1,9 +1,12 @@
-import { plugin, segment } from 'alemon'
+import {
+	plugin,
+	segment
+} from 'alemon'
 
 export class ercy extends plugin {
 	constructor() {
 		super({
-			dsc: '图片',
+			dsc: '随机图片',
 			rule: [{
 					reg: "^/tag.*$", //二次元图片，可后面接标签，比如/tag萝莉
 					fnc: 'tag'
@@ -24,10 +27,10 @@ export class ercy extends plugin {
 					reg: "^/三次元.*$",
 					fnc: 'sanyc' //随机三次元图片
 				},
-				/*{
-					reg: "^/三次元r18.*$",
-					fnc: 'sanycr18' //随机三次元r18+图片
-				},*/
+				/*		{
+						reg: "^/三次元r18.*$",
+						fnc: 'sanycr18' //随机三次元r18+图片
+					},*/
 			]
 		})
 	}
@@ -38,23 +41,26 @@ export class ercy extends plugin {
 		const tagContent = e.msg.content.replace(/^(<@!\d+>\s*)?\/tag\s*/, '');
 		console.log(tagContent);
 		let flag = 0;
+		let flagA = 0;
 		let url = [
-		`https://api.lolicon.app/setu/v2?tag=${tagContent}&size=original`,
-		`https://api.lolicon.app/setu/v2?tag=${tagContent}&size=regular`,
-		`https://api.lolicon.app/setu/v2?tag=${tagContent}&size=small`
+			`https://api.lolicon.app/setu/v2?tag=${tagContent}&size=original`,
+			`https://api.lolicon.app/setu/v2?tag=${tagContent}&size=regular`,
+			`https://api.lolicon.app/setu/v2?tag=${tagContent}&size=small`
 		]
 		let imageUrl;
-		while (flag < 2) {
-		const randomNum = Math.floor(Math.random() * url.length);
+		while (flag < 2 && flagA < 50) {
+			const randomNum = Math.floor(Math.random() * url.length);
 			try {
 				const response = await fetch(url[randomNum]);
 				const data = await response.json(); // 解析 JSON 数据
-				if(randomNum==0){
-				 imageUrl = data.data[0].urls.original;
-				}else if(randomNum==1){
-				 imageUrl = data.data[0].urls.regular;
-				}else{imageUrl = data.data[0].urls.small;}
-				
+				if (randomNum == 0) {
+					imageUrl = data.data[0].urls.original;
+				} else if (randomNum == 1) {
+					imageUrl = data.data[0].urls.regular;
+				} else {
+					imageUrl = data.data[0].urls.small;
+				}
+
 				const res = await e.reply(segment.at(e.msg.author.id), segment.image(imageUrl));
 				if (res === true) {
 					flag++;
@@ -72,19 +78,22 @@ export class ercy extends plugin {
 					console.log(flag);
 				}
 			}
-
+			flagA++;
 		}
-
+		if (flagA >= 50) {
+			e.reply(segment.at(e.msg.author.id), "没找到图，再来一次吧！");
+		}
 		return false;
 
 	}
 
 
 	async setu(e) {
-		const tagContent = e.msg.content.replace(/^(<@!\d+>\s*)?\/18\s*/, '');
+		const tagContent = e.msg.content.replace(/^(<@!\d+>\s*)?\/r18\s*/, '');
 		console.log(tagContent);
 		let flag = 0;
-		while (flag < 2) {
+		let flagA = 0;
+		while (flag < 2 && flagA < 50) {
 			const randomNum = Math.floor(Math.random() * 2) + 1;
 			if (randomNum == 1) {
 				let url = `https://api.lolicon.app/setu/v2?tag=${tagContent}&r18=1`;
@@ -106,8 +115,13 @@ export class ercy extends plugin {
 				}
 			}
 
+			flagA++;
+		}
+		if (flagA >= 50) {
+			e.reply(segment.at(e.msg.author.id), "没找到图，再来一次吧！");
 		}
 		return false;
+
 	}
 
 
@@ -120,7 +134,8 @@ export class ercy extends plugin {
 		];
 
 		let flag = 0;
-		while (flag < 2) {
+		let flagA = 0;
+		while (flag < 2 && flagA < 50) {
 			const randomNum = Math.floor(Math.random() * urls.length);
 			const res = await e.reply(segment.at(e.msg.author.id), segment.image(urls[randomNum]));
 			if (res === true) {
@@ -144,7 +159,8 @@ export class ercy extends plugin {
 		];
 
 		let flag = 0;
-		while (flag < 2) {
+		let flagA = 0;
+		while (flag < 2 && flagA < 50) {
 			const randomNum = Math.floor(Math.random() * urls.length);
 			let resA = await fetch(urls[randomNum])
 			console.log(resA.url);
@@ -153,26 +169,31 @@ export class ercy extends plugin {
 				flag++;
 				console.log(flag);
 			}
+			flagA++;
 		}
-
+		if (flagA >= 50) {
+			e.reply(segment.at(e.msg.author.id), "没找到图，再来一次吧！");
+		}
 		return false;
+
 	}
 
 
 	async sanyc(e) {
 		const urls = [
-		"http://dsyai.club/api/youhuotu.php",
-		"https://cdn.seovx.com/?mom=302",
-		"http://api.yujn.cn/api/yangyan.php",
-		"http://api.yujn.cn/api/yht.php",
-		"http://api.yujn.cn/api/cos.php",
-		"http://api.yujn.cn/api/xjjtp.php?",
-		"http://api.yujn.cn/api/jk.php?",
-		"http://api.yujn.cn/api/yscos.php?"
+			"http://dsyai.club/api/youhuotu.php",
+			"https://cdn.seovx.com/?mom=302",
+			"http://api.yujn.cn/api/yangyan.php",
+			"http://api.yujn.cn/api/yht.php",
+			"http://api.yujn.cn/api/cos.php",
+			"http://api.yujn.cn/api/xjjtp.php?",
+			"http://api.yujn.cn/api/jk.php?",
+			"http://api.yujn.cn/api/yscos.php?"
 		];
 
 		let flag = 0;
-		while (flag < 2) {
+		let flagA = 0;
+		while (flag < 2 && flagA < 50) {
 			const randomNum = Math.floor(Math.random() * urls.length);
 			let resA = await fetch(urls[randomNum])
 			console.log(resA.url);
@@ -181,11 +202,15 @@ export class ercy extends plugin {
 				flag++;
 				console.log(flag);
 			}
+			flagA++;
 		}
-
+		if (flagA >= 50) {
+			e.reply(segment.at(e.msg.author.id), "没找到图，再来一次吧！");
+		}
 		return false;
+
 	}
-	
+
 	async sanycr18(e) {
 		const urls = [
 			"https://imagesapi.sesepic.top/3r18",
@@ -193,16 +218,21 @@ export class ercy extends plugin {
 		];
 
 		let flag = 0;
-		while (flag < 2) {
+		let flagA = 0;
+		while (flag < 2 && flagA < 50) {
 			const randomNum = Math.floor(Math.random() * urls.length);
 			const res = await e.reply(segment.at(e.msg.author.id), segment.image(urls[randomNum]));
 			if (res === true) {
 				flag++;
 				console.log(flag);
 			}
+			flagA++;
 		}
-
+		if (flagA >= 50) {
+			e.reply(segment.at(e.msg.author.id), "没找到图，再来一次吧！");
+		}
 		return false;
+
 	}
 
 
